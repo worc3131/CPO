2e32974194281fe162dbc7a053f9f850d9e7c240
George <web.gwh@gmail.com>
2020-11-15 18:55:03 +0000

    dining philosophers demo

--- demos/demo.py
+++ demos/demo.py
@@ -1,7 +1,9 @@
 
+from . import demo_philosophers
 from . import demo_pi
 from . import demo_primes
 
 if __name__ == '__main__':
+    demo_philosophers.main()
     demo_pi.main()
     demo_primes.main()
---
+++ demos/demo_philosophers.py
@@ -0,0 +1,48 @@
+
+import random
+import time
+
+from cpo import *
+
+def run_demo():
+    """ Dining philosophers """
+    # our philosophers use chopsticks so as to not
+    # confuse their forks and their forks
+    N = 20
+    RUN_FOR = 0.5  # seconds
+
+    chopsticks = [SimpleLock(True, f'chopstick {i}') for i in range(N)]
+
+    stop = False
+    times_ate = [0]*N
+    @fork_procs(range(N))
+    @repeat
+    def philosopher(i):
+        if stop:
+            raise Stopped
+        l, r = i, (i+1) % N
+        if random.random() < 0.5:
+            l, r = r, l
+        chopsticks[l].lock()
+        chopsticks[r].lock()
+        times_ate[i] += 1
+        chopsticks[r].unlock()
+        chopsticks[l].unlock()
+
+    time.sleep(RUN_FOR)
+
+    stop = True
+    for c in chopsticks:
+        c.cancel()
+
+    return times_ate
+
+def main():
+    print(
+        'Here is the spaghetti bill from the philosophers: ',
+        run_demo(),
+        '. Descarte looks like he is still hungry',
+    )
+
+if __name__ == '__main__':
+    main()
