3aec3131dbe00aa6a56c34b86ab854440e4303f3
George <web.gwh@gmail.com>
2020-11-20 00:23:16 +0000

    Test flag

--- cpo/tests/test_flag.py
+++ cpo/tests/test_flag.py
@@ -1,5 +1,62 @@
 
+import pytest
+import time
+
 from cpo import *
 
 def test_flag_init():
     Flag()
+
+def test_flag():
+    f = Flag()
+    done = False
+    @fork_proc
+    def p():
+        nonlocal done
+        f.acquire()
+        done = True
+    time.sleep(0.1)
+    assert not done
+    f.release()
+    time.sleep(0.1)
+    assert done
+
+def test_flag_acquire_once():
+    f = Flag()
+    fork_proc(lambda: f.acquire())
+    with pytest.raises(Exception):
+        f.acquire()
+    with pytest.raises(Exception):
+        f.try_acquire(Nanoseconds.from_seconds(1))
+
+def test_flag_release_once():
+    f = Flag()
+    f.release()
+    with pytest.raises(Exception):
+        f.release()
+
+def test_flag_stays_released():
+    f = Flag()
+    f.release()
+    f.acquire()
+    f.acquire()
+    f.acquire()
+    @procs(range(5))
+    def workers(i):
+        f.acquire()
+    workers()
+
+def test_flag_try_acquire():
+    f = Flag()
+    assert not f.try_acquire(Nanoseconds.from_seconds(0.01))
+    assert not f.try_acquire(Nanoseconds.from_seconds(0.01))
+    @fork_proc
+    def p():
+        time.sleep(0.2)
+        f.release()
+    assert not f.try_acquire(Nanoseconds.from_seconds(0.15))
+    assert f.try_acquire(Nanoseconds.from_seconds(0.15))
+    assert f.try_acquire(Nanoseconds.from_seconds(0.15))
+    f.acquire()
+
+
