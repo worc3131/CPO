24595decf32facc887f9a5e146a68628ddc82711
George <web.gwh@gmail.com>
2020-12-09 17:41:39 +0000

    Use demos for integration tests

--- cpo/tests/test_integration.py
+++ cpo/tests/test_integration.py
@@ -1,5 +1,20 @@
 
-from cpo import *
+from demos import *
 
-def test():
-    pass
+def test_demo_clock():
+    demo_clock.run_demo_and_check()
+
+def test_demo_inc():
+    demo_inc.run_demo()
+
+def test_demo_philosophers():
+    demo_philosophers.run_demo()
+
+def test_demo_pi():
+    demo_pi.run_demo()
+
+def test_demo_primes():
+    demo_primes.run_demo()
+
+def test_demo_readers_writers():
+    demo_readers_writers.run_demo()
